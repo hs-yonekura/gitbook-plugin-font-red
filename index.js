835a4532a0f4b207ec4cff0eba4c2a55eca4052f
hs-yonekura <y-yonekura@science.co.jp>
2019-11-20 16:05:24 +0900

    cheerio処理修正

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 var cheerio = require('cheerio');
-var re = /(^@|\s@).*?(@$|@\s)/;
+var re = /(^@|\s@).*?(@$|@\s)/g;
+
+function replacer($, text) {
+  if ($(text).children().length) {
+    $(text).children().each(function (itm) {
+      return replacer($, $(this));
+    });
+  }
+  else {
+    var value = $(text).text();
+    value = value.replace(re, '<font color="red">$2</font>');
+    return $(text).text(value);
+  }
+}
+
+function f(body) {
+  var $ = cheerio.load(body);
+  var bb = $("*").each(function (itm) {
+    return replacer($, $(this));
+  });
+  return $.html(bb);
+}
 
 module.exports = {
   hooks: {
     page: function (page) {
-      let $ = cheerio.load(page.content);
-      $("*").each(function (index, a) {
-        var a = $(a);
-        var text = a.text();
-        if (re.test(text)) {
-          text = text.replace(/(^@|\s@)(.*?)(@$|@\s)/, '<font color="red">$2</font>');
-          $(a).text(text);
-        }
-      }
+      page.content = f(page.content).replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"');
+      return page;
     }
   }
 }
