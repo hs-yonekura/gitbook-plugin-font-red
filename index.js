013fd594351589c0f129139251408babbe91bd9d
hs-yonekura <y-yonekura@science.co.jp>
2019-11-21 13:39:12 +0900

    bug fixed

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-const cheerio = require('cheerio');
-const re = /(^@|\s@)(.*?)(@$|@\s)/g;
-
-function replacer($, text) {
-  if ($(text).children().length) {
-    $(text).children().each(function (itm) {
-      return replacer($, $(this));
-    });
-  }
-  else {
-    var value = $(text).text();
-    value = value.replace(re, '<font color="red">$2</font>');
-    return $(text).text(value);
-  }
-}
-
-function f(body) {
-  var $ = cheerio.load(body);
-  var bb = $("*").each(function (itm) {
-    return replacer($, $(this));
-  });
-  return $.html(bb);
-}
-
 module.exports = {
-  hooks: {
-    'page': function(page) {
-      page.content = f(page.content).replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"');
-      return page;
+  blocks: {
+    fontred: {
+      shortcuts: {
+        parsers: ['markdown', 'asciidoc', 'restructuredtext'],
+        start:   '@',
+        end:     '@'
+      },
+      process(content) {
+        return '<font color="red">' + content.body + '</font>';
+      }
     }
   }
 }
